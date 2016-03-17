1700e2dd01b304d10e12666b33ae0f1a467c5858
Sittipong Jungsakul <stp55160555@gmail.com>
2016-03-17 23:25:53 +0700

    สร้าง controllers สำหรับจัดการPath Search,Register

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,14 +2,27 @@ package controllers
 
 import "github.com/revel/revel"
 
+//App for save Structure of Folder App (in views)
 type App struct {
 	*revel.Controller
 }
 
+//Search for save Structure of Folder Search (in views)
+type Search struct {
+	*revel.Controller
+}
+
+//Index for Create routing Page Index (localhost/index)
 func (c App) Index() revel.Result {
 	return c.Render()
 }
 
+//SearchPlant for Create routing Page Index (localhost/searchplant)
+func (c Search) SearchPlant() revel.Result {
+	return c.Render()
+}
+
+//Register for Create routing Page Register (localhost/register)
 func (c App) Register() revel.Result {
 	return c.Render()
 }
