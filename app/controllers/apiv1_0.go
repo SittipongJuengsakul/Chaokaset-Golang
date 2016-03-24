e46d944d4b77f3067a6e223ef3ef9c71e6bc875c
Sittipong Jungsakul <stp55160555@gmail.com>
2016-03-24 19:48:37 +0700

    เพิ่มส่วน Api Register

--- app/controllers/apiv1_0.go
+++ app/controllers/apiv1_0.go
@@ -4,7 +4,7 @@ import (
     "github.com/revel/revel"
     //"github.com/gocql/gocql"
 		"chaokaset-go/app/models"
-    //"golang.org/x/crypto/bcrypt"
+    "golang.org/x/crypto/bcrypt"
 )
 
 //Auth for save Structure of Folder Authen (in views)
@@ -28,4 +28,16 @@ func (c Api) CheckLogin(Username string,Password string) revel.Result {
     }
     R = &ResAuth{Status: res,UserData: U}
     return  c.RenderJson(R)
+}
+func (c Api) RegisterUser(Username string,Password string,Prefix string,Name string,Lname string,Tel string) revel.Result {
+  var R *ResAuth
+  var U *models.User
+  HashedPassword, _ := bcrypt.GenerateFromPassword(
+    []byte(Password), bcrypt.DefaultCost)
+  res := models.RegisterUserChaokaset(Username ,HashedPassword,Prefix ,Name ,Lname ,Tel);
+  if res {
+    U = models.GetUserData(Username)
+  }
+  R = &ResAuth{Status: res,UserData: U}
+  return  c.RenderJson(R)
 }
