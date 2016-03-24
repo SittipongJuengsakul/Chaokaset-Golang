4fd705b366ac91555d19cfdfa3a27b932dee12d8
Sittipong Jungsakul <stp55160555@gmail.com>
2016-03-24 17:44:36 +0700

    เพิ่มส่วยของ api

--- app/controllers/apiv1_0.go
+++ app/controllers/apiv1_0.go
@@ -4,5 +4,23 @@ import (
     "github.com/revel/revel"
     //"github.com/gocql/gocql"
 		"chaokaset-go/app/models"
-    "golang.org/x/crypto/bcrypt"
+    //"golang.org/x/crypto/bcrypt"
 )
+
+//Auth for save Structure of Folder Authen (in views)
+type Api struct {
+	*revel.Controller
+}
+
+
+func (c Api) Index(user *models.User) revel.Result {
+	return c.Render()
+}
+func (c Api) CheckLogin(Username string,Password string) revel.Result {
+    res := models.CheckPasswordUser(Username,Password)
+    if res {
+      return  c.RenderJson(res)
+    } else {
+      return  c.RenderJson(res)
+    }
+}
