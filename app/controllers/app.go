01d429ac3509aa7048004c05ae09e70313d276c0
Sittipong Jungsaskul <stp55160555@gmail.com>
2016-04-16 16:35:14 +0700

    เพิ่มหน้า view จัดการการเกษตร

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -57,6 +57,7 @@ func init() {
   revel.InterceptFunc(setuser, revel.BEFORE, &Profile{})
   revel.InterceptFunc(setuser, revel.BEFORE, &Howto{})
   revel.InterceptFunc(setuser, revel.BEFORE, &Plan{})
+  revel.InterceptFunc(setuser, revel.BEFORE, &Management{})
   revel.InterceptFunc(checksetuser, revel.BEFORE, &Crops{})
   revel.InterceptFunc(checksetuser, revel.BEFORE, &Profile{})
 }
