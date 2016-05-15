fa520c5f48717be0bc9865d8e5ca37594c8ce2fe
Teerasak Boonserm <manu98765@hotmail.com>
2016-05-15 04:42:29 -0400

    ปิด/เปิดการขาย และเพิ่มหน้าแก้ไขสินค้า

--- app/controllers/sell.go
+++ app/controllers/sell.go
@@ -5,14 +5,7 @@ import (
 	  "chaokaset-go/app/models"
     //"net/http"
     "io"
-    "bytes"
-    "mime/multipart"
-    "fmt"
-    "os"
-    "net/http"
-    "io/ioutil"
 
-//  "github.com/nfnt/resize"
 )
 
 //Auth for save Structure of Folder Sell (in views)
@@ -24,7 +17,7 @@ type Sell struct {
 type PostSell struct{
   Name            string
   Category        string
-  Pic             []byte
+  Pic             io.Reader
   Price           int
   Unit            string
   Detail          string
@@ -50,8 +43,7 @@ func (c Sell) Sell() revel.Result {
   return c.Render()
 }
 
-
-func (c Sell) PostSell(sell PostSell,file io.Reader) revel.Result {
+func (c Sell) PostSell(sell PostSell) revel.Result {
   /*c.Validation.Required(sell.Name).Message("จำเป็นต้องกรอก ชื่อสินค้า")
   c.Validation.Required(sell.Price).Message("จำเป็นต้องกรอก ราคาสินค้าเป็นตัวเลข")
   c.Validation.Required(sell.Unit).Message("จำเป็นต้องกรอก หน่วยสินค้า")
@@ -65,102 +57,23 @@ func (c Sell) PostSell(sell PostSell,file io.Reader) revel.Result {
     return c.Redirect(Sell.Sell)
   }*/
  // headers := c.Params.Files["sell.Pic"]
-
- // headers := c.Params.Files["file"]
- 
-   // target_url := "http://188.166.245.68:9000/public/img/pic"
-    //filename := "public/img/pic"
-    //postFile(headers, target_url)
+   headers := c.Params.Files["sell.Pic"]
   //out io.Writer 
   //out := "/public/img/sell/555.jpg" 
   //out := os.OpenFile("./public/img/sell/555.jpg", 0666)
-  //io.Copy(fileWriter,fn)
-
-  //err := c.Params.Files["file"]
- // e := os.File("file")
-  //f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0777)
-        
-       // fmt.Fprintf(w, "%v", handler.Header)
-      /*  f, err := os.OpenFile("./public/img/pic", os.O_WRONLY|os.O_CREATE, 0666)
-        if err != nil {
-            panic(err)
-        }
-        defer f.Close()*/
-     //m := c.Request.MultipartForm
- 
+  //err := io.Copy()
   
-       //  m :=c.Params.Files["file"]
-  /*       headers, ok := c.Params.Files["file"]
-  if ok {
-
-    var (
-      err         error
-      //bildBuf     bytes.Buffer
-      nameSplit   []string
-      suffix      string
-      uploadDaten image.Image
-    )
-
-    nameSplit = strings.Split(strings.ToLower(headers[0].Filename), ".")
-    if len(nameSplit) <= 1 {
-      //return c.RenderError(SaveFotoError{msg: "no file suffix."})
-      return c.RenderJson(1111111111)
-    }
-    suffix = nameSplit[len(nameSplit)-1]
-
-    switch {
-    case suffix == "jpg" || suffix == "jpeg":
-      uploadDaten, err = jpeg.Decode(file)
-      if err != nil {
-        panic(err)
-       // return c.RenderError(SaveFotoError{"jpeg.Decode() failed", err})
-      }
-    case suffix == "png":
-      uploadDaten, err = png.Decode(file)
-      if err != nil {
-        panic(err)
-      }
-    case suffix == "gif":
-      uploadDaten, err = gif.Decode(file)
-      if err != nil {
-        panic(err)
-      }
-
-   resized := resize.Resize(500, 0, uploadDaten, resize.Lanczos3)
-
-    err = png.Encode(&bildBuf, resized)
-    if err != nil {
-      panic(err)
-    }
-    f.Data = bildBuf.Bytes()
-    f.Width = resized.Bounds().Dx()
-    f.Height = resized.Bounds().Dy()
-return c.RenderJson(uploadDaten)
-  }}*/
-
-        
-        //io.Copy(f, io.Reader(c.Params.Files["file"]))
-  //target_url := "http://188.166.245.68:9000/" 
-  body_buf := bytes.NewBufferString("") 
-  body_writer := multipart.NewWriter(body_buf) 
-  filename := "rice1.jpg" 
-  file_writer, err := body_writer.CreateFormFile("file", filename) 
-  if err != nil { 
-   panic(err)
-  }
-
-  return c.RenderJson(file_writer)
+  return c.RenderJson(headers)
   
  /* data := models.GetUserid(c.Session["username"])
   
-  err := models.AddSellData2(sell.Name,sell.Category,sell.Price,sell.Unit,sell.Detail,sell.Expire,data.Userid.Hex(),13,100,2)
+  err := models.AddSellData2(sell.Name,sell.Category,sell.Price,sell.Unit,sell.Detail,sell.Expire,data.Userid.Hex(),13,100)
   if err {
       return  c.Redirect(Sell.IndexSell)
     } else {
       c.Flash.Error("เกิดข้อผิดพลาดไม่สามารถขายสินค้าได้ กรุณากรอกข้อมูลใหม่!!")
       return c.Redirect(Sell.Sell)
     }*/
-
 }
 
 func (c Sell) ManageSell() revel.Result {
@@ -170,75 +83,20 @@ func (c Sell) ManageSell() revel.Result {
   return c.Render(data)
 }
 
-func postFile(filename string, targetUrl string) error {
-    bodyBuf := &bytes.Buffer{}
-    bodyWriter := multipart.NewWriter(bodyBuf)
-
-    // this step is very important
-    fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
-    if err != nil {
-        fmt.Println("error writing to buffer")
-        return err
-    }
-
-    // open file handle
-    fh, err := os.Open(filename)
-    if err != nil {
-        fmt.Println("error opening file")
-        return err
-    }
-
-    //iocopy
-    _, err = io.Copy(fileWriter, fh)
-    if err != nil {
-        return err
-    }
-
-    contentType := bodyWriter.FormDataContentType()
-    bodyWriter.Close()
-
-    resp, err := http.Post(targetUrl, contentType, bodyBuf)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-    resp_body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
-    fmt.Println(resp.Status)
-    fmt.Println(string(resp_body))
-    return nil
+func (c Sell) EditProductSell(idSell string) revel.Result {
+  data := models.GetSellDetail(idSell)
+  return c.Render(data)
 }
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+func (c Sell) PostEditSell() revel.Result {
+  return nil
+}
 
-  // the FormFile function takes in the POST input id file
-  file, header, err := r.FormFile("sell.Pic")
-
-  if err != nil {
-    fmt.Fprintln(w, err)
-    return
-  }
-
-  defer file.Close()
-
-  out, err := os.Create("/public/img/pic")
-  if err != nil {
-    fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
-    return
-  }
-
-  defer out.Close()
-
-  // write the content from POST to the file
-  _, err = io.Copy(out, file)
-  if err != nil {
-    fmt.Fprintln(w, err)
-  }
-
-  fmt.Fprintf(w, "File uploaded successfully : ")
-  fmt.Fprintf(w, header.Filename)
-  //return w
- }
-
+func (c Sell) CloseSell(idSell string) revel.Result {
+  models.UpdateStatusSell(idSell,0)
+  return  c.Redirect(Sell.ManageSell)
+}
+func (c Sell) OpenSell(idSell string) revel.Result {
+  models.UpdateStatusSell(idSell,1)
+  return  c.Redirect(Sell.ManageSell)
+}
