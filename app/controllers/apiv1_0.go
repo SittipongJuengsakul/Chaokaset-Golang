91c18306fd30a9eb8cd8595e1404893b96e254e6
Sittipong Jungsaskul <stp55160555@gmail.com>
2016-05-17 18:38:16 +0700

    ลบคอมมเม้นไม่ไช้ออก

--- app/controllers/apiv1_0.go
+++ app/controllers/apiv1_0.go
@@ -1,12 +1,8 @@
 package controllers
 import (
     "github.com/revel/revel"
-    //"github.com/gocql/gocql"
-    //"gopkg.in/mgo.v2"
-   // "gopkg.in/mgo.v2/bson"
     "chaokaset-go/app/models"
     "golang.org/x/crypto/bcrypt"
-   // "time"
 )
 
 type Api struct {
